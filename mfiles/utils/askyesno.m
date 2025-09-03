29396084dc51cff1037001f5d99ebf59e524c797
louisnar <olivier.louisnard@mines-albi.fr>
2025-09-03 14:52:12 +0200

    askyesno.m was commented and enhanced in 0LUTILS (this is a rough copy of the latter...)

--- mfiles/utils/askyesno.m
+++ mfiles/utils/askyesno.m
@@ -1,9 +1,37 @@
 % ASKYESNO :
 %
-% Asks a question  and waits a n answer yes or no. On output,
-% answer is 1 if answer if yes, 0 otherwise. 
+% Asks a question  and waits until the user presses a key
+% interpreted as  yes or no. 
+%
+% Usage:
+%     answercode = askyesno (message, yesanswers, noanswers, RETURN_IS_YES)
+% On input:
+%
+%   message: message to display
+%
+%   yesanswers:  either string or string cell containing all
+%                possible strings accepted as "yes"
+%
+%   noanswers:  either string or string cell containing all
+%                possible strings accepted as "no"
+%
+%   RETURN_IS_YES (optional):
+%       set to 1 if you want RETURN key pressed understood as yes
+%       set to 0 if you want RETURN key pressed understood as no (default)
+%       set to -1 if you want to leave RETURN interpreted as
+%         'other' (strict mode)
+%
+% On output:
+%       answercode is 1    if answer if yes, 
+%                     0    if answer is no; 
 
-function answer = askyesno (message, yesanswers, noanswers, RETURN_IS_YES)
+% an
+%
+% Example: a typical use may be :
+%
+%     askyesno('Yes or no?  ', {'Y','y'}, {'N', 'n'}, 1)
+
+function answercode = askyesno (message, yesanswers, noanswers, RETURN_IS_YES)
 
 % bBy default, return key is understood as "no"
 if nargin < 4 || isempty(RETURN_IS_YES)
@@ -17,24 +45,34 @@ while ~done
   answer = input(message, 's');
   
   if isempty ( answer )  
-    % User enters ENTER key
-    answer = RETURN_IS_YES ;
+      % User enters ENTER key
+    if RETURN_IS_YES == -1
+      % We are in strict mode, thus this is unacceptable
+      done = 0 ;
+    elseif RETURN_IS_YES == 0 || RETURN_IS_YES == 1
+      % We are not in strict mode, interpret as yes or no
+      answercode = RETURN_IS_YES ;
+      done = 1 ;
+    else 
+      % RETURN_IS_YES has bad value
+      error('Bad value for input argument RETURN_IS_YES \n') ;
+    end
     
-    done = 1 ;
-  else 
+  else
     switch answer
       
      case yesanswers
       % User enters YES
-      answer = 1 ;
+      answercode = 1 ;
       done  = 1 ;
       
      case noanswers
       % User enters NO
-      answer = 0 ;
+      answercode = 0 ;
       done  = 1 ;
       
      otherwise
+      % Cannot exit loop if answer is neither yes nor no
       done = 0  ;
     end
   end
